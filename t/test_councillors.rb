4f4b77d5216025588a3b0a52f7651430fbdcdf4c
Luke Bacon <luke@equivalentideas.com>
2016-02-15 07:24:56 +1100

    Add test to make sure councillors are assign to correct council

---
+++ t/test_councillors.rb
@@ -0,0 +1,22 @@
+require 'csv_to_popolo'
+require 'minitest/autorun'
+require 'json'
+
+describe 'councillors' do
+  subject     { Popolo::CSV.new('t/data/councillors.csv') }
+  let(:organizations)  { subject.data[:organizations] }
+
+  describe 'Patricia Gould' do
+    let(:patricia) { subject.data[:persons][6] }
+    let(:memberships) do
+      subject.data[:memberships].select { |m| m[:person_id] == patricia[:id] }
+    end
+
+    it 'is associated with the correct council' do
+      leg_mem = memberships.find { |m| m[:role] == 'member' }
+      member_org = organizations.find { |o| o[:id] == leg_mem[:on_behalf_of_id] }
+
+      member_org[:name].must_equal 'Albury City Council'
+    end
+  end
+end
